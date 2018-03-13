06b79ab3d6cc90c2b5d4028d5f97bc2c28c5a82d
Aaron Gooden <aagooden@me.com>
2018-03-13 15:20:53 -0400

    Initial tests passing

---
+++ coin_changer.rb
@@ -0,0 +1,4 @@
+def changer(cents)
+	change = {}
+	return change
+end
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -1,10 +1,16 @@
 require "minitest/autorun"
-require_relative "coin_flip.rb"
+require_relative "coin_changer.rb"
 
 class Coin_flip_test < Minitest::Test
 
 	def test_boolean
 		assert_equal(true, true)
 	end
+
+	def test_hash_return
+		cents = 50
+		assert_equal(Hash, changer(cents).class)
+	end
+
 
 end
